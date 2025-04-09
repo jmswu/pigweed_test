57d8a2d9a8fffeaa0997d992d44395a09518a798
James Wu <j.wu@mstglobal.com>
2025-04-09 19:08:12 +1000

    update

--- main.cpp
+++ main.cpp
@@ -1,10 +1,13 @@
 #include <cstdio>
 #include <cstdlib>
+#include "pw_log/log.h"
 
 int main()
 {
 
     std::printf("Hello world\n");
+
+    PW_LOG_INFO("Hello world from pigweed...");
 
     return EXIT_SUCCESS;
 }
